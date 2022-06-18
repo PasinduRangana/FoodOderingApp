268f01f14e4bd3f2db78a65cfd3e085f407577be
Pasindu <pasindur@innosoftwareworld.com>
2022-06-18 17:50:56 +0530

    meal items and card component

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import calasses from './AvailableMeals.module.css';
-
+import MealItem from './MealItem/MealItem';
+import Card from '../UI/Card';
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -28,11 +29,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meals) => <li>{meals.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meals) => (
+    <MealItem
+      key={meals.id}
+      name={meals.name}
+      description={meals.description}
+      price={meals.price}
+    />
+  ));
 
   return (
     <section className={calasses.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
